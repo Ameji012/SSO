66c6dcea0f44e9101c3714f3fab8bb5dcd09f3f2
Anuvis Mejia <ameji012@gmail.com>
2014-12-03 15:33:31 -0500

    reconfigured model associations

--- app/models/matchup.rb
+++ app/models/matchup.rb
@@ -1,5 +1,5 @@
 class Matchup < ActiveRecord::Base
-
+	belongs_to :round
 	belongs_to :home_player, class_name: "Player"
 	belongs_to :away_player, class_name: "Player"
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,5 @@
 class Player < ActiveRecord::Base
-	
-
 	has_many :scores
 	has_many :home_matchups, class_name: "Matchup", foreign_key: :home_player_id
 	has_many :away_matchups, class_name: "Matchup", foreign_key: :away_player_id
-
-
 end 
