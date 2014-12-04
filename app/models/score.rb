84cbe9721dd672591da52af5f0784c25301a64b1
Isaac Weinbach <isaacito@aol.com>
2014-12-03 21:40:16 -0500

    fix

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,8 +2,8 @@ class Score < ActiveRecord::Base
   belongs_to :player
   # after_save :check_score
 
-  def check_score
-    player.matchup.tally([player, score])
-  end
+  # def check_score
+  #   player.matchup.tally([player, score])
+  # end
 
 end
